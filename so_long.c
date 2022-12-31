8c6357ac965a96aa2b4a859c6d558edca964b8d8
Marwane Ahansal <ahansalmarwane@gmail.com>
2022-12-31 02:36:48 +0100

    added player, collectable and an exit images

--- so_long.c
+++ so_long.c
@@ -6,7 +6,7 @@
 /*   By: mahansal <mahansal@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/16 15:15:51 by mahansal          #+#    #+#             */
-/*   Updated: 2022/12/31 01:08:21 by mahansal         ###   ########.fr       */
+/*   Updated: 2022/12/31 02:35:06 by mahansal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -53,10 +53,17 @@ int main(int argc, char *argv[])
 	mlx_window = mlx_new_window(mlx, game->nbr_rows * 46, game->nbr_cols * 45, "So Long!");
 	void	*ground_img;
 	void	*wall_img;
+	void	*player_img;
+	void	*collectable_img;
+	void	*exit_img;
 	int		img_height;
 	int		img_width;
 	ground_img = mlx_xpm_file_to_image(mlx, "./assets/ground.xpm", &img_width, &img_height);
 	wall_img = mlx_xpm_file_to_image(mlx, "./assets/wall.xpm", &img_width, &img_height);
+	player_img = mlx_xpm_file_to_image(mlx, "./assets/player.xpm", &img_width, &img_height);
+	collectable_img = mlx_xpm_file_to_image(mlx, "./assets/collectable.xpm", &img_width, &img_height);
+	exit_img = mlx_xpm_file_to_image(mlx, "./assets/exit.xpm", &img_width, &img_height);
+
 	if (!ground_img || !wall_img)
 		printf("img error!\n");
 	int i = 0;
@@ -70,6 +77,12 @@ int main(int argc, char *argv[])
 				mlx_put_image_to_window(mlx, mlx_window, ground_img, j * img_width, i * img_height);
 			else if (game->map_2d[i][j] == '1')
 				mlx_put_image_to_window(mlx, mlx_window, wall_img, j * img_width, i * img_height);
+			else if (game->map_2d[i][j] == 'P')
+				mlx_put_image_to_window(mlx, mlx_window, player_img, j * img_width, i * img_height);
+			else if (game->map_2d[i][j] == 'C')
+				mlx_put_image_to_window(mlx, mlx_window, collectable_img, j * img_width, i * img_height);
+			else if (game->map_2d[i][j] == 'E')
+				mlx_put_image_to_window(mlx, mlx_window, exit_img, j * img_width, i * img_height);
 			j++;
 		}
 		i++;
